0561d1690ef7e2a5a02fe51ee2301401a57b37c1
dlam <dryanlam@gmai.com>
2021-05-17 14:49:42 +0700

    REFACTORING code

--- Include/scripts/groovy/com/dryan/hook/Hooks.groovy
+++ Include/scripts/groovy/com/dryan/hook/Hooks.groovy
@@ -6,8 +6,9 @@ import cucumber.api.Scenario
 import cucumber.api.java.After
 import cucumber.api.java.Before
 import dl.rest.com.ApiClient
-import dl.rest.com.WSResponse
 import dl.rest.com.Parser
+import dl.rest.com.WSResponse
+import internal.GlobalVariable
 
 
 public class Hooks {
@@ -19,7 +20,10 @@ public class Hooks {
 	@Before
 	def installApp(Scenario sc) {
 		println "Before"
-		client = new ApiClient()
+		def apiUrl = GlobalVariable.API_URL
+
+		client = new ApiClient(apiUrl)
+		response = new WSResponse()
 
 		def tags = sc.getSourceTagNames().join(",")
 		client.setHeader(["testcase": tags])
--- Include/scripts/groovy/com/dryan/step/APISteps.groovy
+++ Include/scripts/groovy/com/dryan/step/APISteps.groovy
@@ -8,29 +8,10 @@ import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 
 class APISteps extends Hooks{
-	//	/**
-	//	 * The step definitions below match with Katalon sample Gherkin steps
-	//	 */
-	//	@Given("I want to write a step with (.*)")
-	//	def I_want_to_write_a_step_with_name(String name) {
-	//		println name
-	//	}
-	//
-	//	@When("I check for the (\\d+) in step")
-	//	def I_check_for_the_value_in_step(int value) {
-	//		println value
-	//	}
-	//
-	//	@Then("I verify the (.*) in step")
-	//	def I_verify_the_status_in_step(String status) {
-	//		println status
-	//	}
-
-	//---
-		 
+	
 	@Given(/^We use default header$/)
 	def we_use_default_header(){
-		
+
 		def defaultHeader = new HashMap<String,Object>()
 		client.setHeader(defaultHeader)
 	}
@@ -42,12 +23,14 @@ class APISteps extends Hooks{
 
 	@When(/^We send a POST request to '(.+?)' endpoint with body:$/)
 	def we_send_a_POST_request_to_endpoint_with_body(String endPoint, String dataDody) {
-		client.doPostRequest(endPoint, dataDody)
+		def res = client.doPostRequest(endPoint, dataDody)
+		response.setResponse(res)
 	}
 
 	@When(/^We send a GET request to '(.+?)' endpoint$/)
 	def we_send_a_GET_request_to_endpoint(String endPoint) {
-		client.doGetRequest(endPoint)
+		def res = client.doGetRequest(endPoint)
+		response.setResponse(res)
 	}
 
 	@Then(/^We got the Response with status code '(.+?)'$/)
