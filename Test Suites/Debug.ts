64367b95d10d9ea8a3ada322a28b498626dad266
dlam <dryanlam@gmai.com>
2021-05-20 16:22:50 +0700

    Added test run for BDD

---
+++ Test Suites/Debug.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>Debug</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3a6d4855-7286-498b-b375-03ed069c4801</testSuiteGuid>
+   <filteringExtension>com.katalon.plugin.tags.AdvancedTagQueryingTestSuiteImpl</filteringExtension>
+   <filteringPlugin>com.katalon.tags-plugin</filteringPlugin>
+   <filteringText>tag=(Regression) </filteringText>
+</FilteringTestSuiteEntity>
