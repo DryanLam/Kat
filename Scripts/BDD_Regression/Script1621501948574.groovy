a5b07f78f94752b6ac6cbb86b33951d71cab79c8
dlam <dryanlam@gmai.com>
2021-05-21 15:23:57 +0700

    Changed global type

--- Scripts/BDD_Regression/Script1621501948574.groovy
+++ Scripts/BDD_Regression/Script1621501948574.groovy
@@ -3,10 +3,13 @@ import internal.GlobalVariable as GlobalVariable
 
 def TAGS_EXECUTION = ""
 
-if(GlobalVariable.IS_PROFILING == true) {
+if(GlobalVariable.IS_PROFILING == "true") {
 	TAGS_EXECUTION = "@Regression"
 } else {
 	TAGS_EXECUTION = GlobalVariable.TAGS
 }
+
+println(GlobalVariable.IS_PROFILING)
+println(TAGS_EXECUTION)
 
 CucumberKW.runFeatureFolderWithTags('Include/features', TAGS_EXECUTION)
